fc1d9c36c9fea909f28933763310c94a59777eb0
hzhi2 <hzhi2@10.175.3.237>
2023-07-18 11:50:05 +0800

    retrieveAllCamcorder()and Item.Java file method toString() and Camcorder.java file method toString()

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -14,8 +14,26 @@ public class Item {
 	public String toString() {
 		
 		// Write your codes here
-		return null;
+		String ItemInfo = String.format("%-10s %-30s %-10s %-10s",
+				assetTag,
+				description, 
+				showAvailability(isAvailable),
+				dueDate);
+		
+		return ItemInfo;
 	}
+	
+	public static String showAvailability(boolean isAvailable) {
+		String avail;
+
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
+	}
+	
 	public String getAssetTag() {
 		return assetTag;
 	}
